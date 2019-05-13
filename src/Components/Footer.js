b9097161e6928a1f1f68e081ca25a4ad27f58a68
SeongBong <chopisal@gmail.com>
2019-05-13 23:38:42 +0900

    footer, auth

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
+
+const Link = styled.a``;
+
+const Copyright = styled.span``;
+
+export default () => <Footer />;
--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,20 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div``;
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+  return (
+    <Wrapper>
+      <Box>box</Box>
+    </Wrapper>
+  );
+};
