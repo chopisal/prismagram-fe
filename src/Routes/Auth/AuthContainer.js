f9354dbc512b6d46b07c0fbcbb84b52ec2f16ff0
SeongBongKim <chopisal@gmail.com>
2019-05-20 14:01:06 +0900

    auth

---
+++ src/Routes/Auth/AuthContainer.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import AuthPresenter from "./AuthPresenter";
+import useInput from "../../Hooks/useInput";
+import { useMutation } from "react-apollo-hooks";
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+  const username = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const secret = useInput("");
+  const email = useInput("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <AuthPresenter
+      setAction={setAction}
+      action={action}
+      username={username}
+      firstName={firstName}
+      lastName={lastName}
+      email={email}
+      secret={secret}
+      onSubmit={onSubmit}
+    />
+  );
+};
