9984b3773bbc9f317696703a734ea701c8ecedbf
SeongBongKim <chopisal@gmail.com>
2019-05-24 09:53:25 +0900

    loader, feed,post

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -6,6 +6,36 @@ import { useQuery } from "react-apollo-hooks";
 import Loader from "../Components/Loader";
 import Post from "../Components/Post";
 
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,11 +44,30 @@ const Wrapper = styled.div`
 `;
 
 export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
   return (
     <Wrapper>
       <Helmet>
         <title>Feed | Prismagram</title>
       </Helmet>
+      {loading && <Loader />}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => (
+          <Post
+            key={post.id}
+            id={post.id}
+            location={post.location}
+            caption={post.caption}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          />
+        ))}
     </Wrapper>
   );
 };
